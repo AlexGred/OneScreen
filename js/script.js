b6deda554dc5eb059d67db4cddea8e2d51dc40c8
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-05-13 18:33:37 +0300

    New fonts. New navbar. Animate nav bar

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+
+js
+
+*/
+
+
+
+( function($) {
+
+  $(document).ready(function() {
+    $('.site-navigation').affix({
+      offset: {
+        top: 40
+      }
+    });
+  });
+
+})(jQuery);
