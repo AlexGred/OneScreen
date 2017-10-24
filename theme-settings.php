23c0fc35747dff98cd88a8ab0ef903183eaac504
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-10-24 23:23:20 +0300

    Code standards fix

--- theme-settings.php
+++ theme-settings.php
@@ -1,12 +1,17 @@
 <?php
+
 /**
- * Implementation of hook_form_system_theme_settings_alter()
+ * @file
+ * Theme setting callbacks for the Adminimal theme.
+ */
+
+/**
+ * Implements hook_form_FORM_ID_alter().
  *
  * @param $form
- *   Nested array of form elements that comprise the form.
- *
+ *   The form.
  * @param $form_state
- *   A keyed array containing the current state of the form.
+ *   The form state.
  */
 
 function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
@@ -19,7 +24,7 @@ function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['onescreen_settings']['show_breadcrumbs'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show breadcrumbs in a page'),
-    '#default_value' => theme_get_setting('show_breadcrumbs','onescreen'),
+    '#default_value' => theme_get_setting('show_breadcrumbs', 'onescreen'),
     '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
   );
   $form['onescreen_settings']['slideshow'] = array(
@@ -31,7 +36,7 @@ function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['onescreen_settings']['slideshow']['slideshow_display'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show slideshow'),
-    '#default_value' => theme_get_setting('slideshow_display','onescreen'),
+    '#default_value' => theme_get_setting('slideshow_display', 'onescreen'),
     '#description'   => t("Check this option to show Slideshow in front page. Uncheck to hide."),
   );
   $form['onescreen_settings']['slideshow']['slide'] = array(
@@ -46,22 +51,22 @@ function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['onescreen_settings']['slideshow']['slide1']['slide1_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide1_head','onescreen'),
+    '#default_value' => theme_get_setting('slide1_head', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide1']['slide1_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide1_desc','onescreen'),
+    '#default_value' => theme_get_setting('slide1_desc', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide1']['slide1_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide1_url','onescreen'),
+    '#default_value' => theme_get_setting('slide1_url', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide1']['slide1_img'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Img'),
-    '#default_value' => theme_get_setting('slide1_img','onescreen'),
+    '#default_value' => theme_get_setting('slide1_img', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide2'] = array(
     '#type' => 'fieldset',
@@ -72,22 +77,22 @@ function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['onescreen_settings']['slideshow']['slide2']['slide2_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide2_head','onescreen'),
+    '#default_value' => theme_get_setting('slide2_head', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide2']['slide2_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide2_desc','onescreen'),
+    '#default_value' => theme_get_setting('slide2_desc', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide2']['slide2_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide2_url','onescreen'),
+    '#default_value' => theme_get_setting('slide2_url', 'onescreen'),
   );
-    $form['onescreen_settings']['slideshow']['slide2']['slide2_img'] = array(
+  $form['onescreen_settings']['slideshow']['slide2']['slide2_img'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Img'),
-    '#default_value' => theme_get_setting('slide1_img','onescreen'),
+    '#default_value' => theme_get_setting('slide1_img', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide3'] = array(
     '#type' => 'fieldset',
@@ -98,22 +103,22 @@ function onescreen_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['onescreen_settings']['slideshow']['slide3']['slide3_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide3_head','onescreen'),
+    '#default_value' => theme_get_setting('slide3_head', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide3']['slide3_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide3_desc','onescreen'),
+    '#default_value' => theme_get_setting('slide3_desc', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slide3']['slide3_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide3_url','onescreen'),
+    '#default_value' => theme_get_setting('slide3_url', 'onescreen'),
   );
-    $form['onescreen_settings']['slideshow']['slide3']['slide3_img'] = array(
+  $form['onescreen_settings']['slideshow']['slide3']['slide3_img'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Img'),
-    '#default_value' => theme_get_setting('slide1_img','onescreen'),
+    '#default_value' => theme_get_setting('slide1_img', 'onescreen'),
   );
   $form['onescreen_settings']['slideshow']['slideimage'] = array(
     '#markup' => t('To change the Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the theme folder.'),
