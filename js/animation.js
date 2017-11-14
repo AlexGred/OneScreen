1513583e50533696eaca15e7ea600672b737cf3e
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-11-14 23:33:11 +0300

    Code standards fix

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@
   'use strict';
 
   $.fn.viewportChecker = function (useroptions) {
-    
+ 
     // Define options and extend with user
     var options = {
       classToAdd: 'visible',
@@ -10,17 +10,17 @@
       scrollBox: window
     };
     $.extend(options, useroptions);
-  
+
     var $elem = this;
     var boxSize = {height: $(options.scrollBox).height(), width: $(options.scrollBox).width()};
-  
+
     /*
       * Main method
       */
     this.checkElements = function () {
       var viewportStart;
       var viewportEnd;
-  
+
       // Set vars to check
       if (!options.scrollHorizontal) {
         viewportStart = Math.max(
@@ -38,41 +38,40 @@
         );
         viewportEnd = (viewportStart + boxSize.width);
       }
-  
+
       // Loop through all given dom elements
       $elem.each(function () {
         var $obj = $(this);
         var objOptions = {};
         var attrOptions = {};
-  
+
         if ($obj.data('vp-add-class')) {
           attrOptions.classToAdd = $obj.data('vp-add-class');
         }
         if ($obj.data('vp-offset')) {
           attrOptions.offset = $obj.data('vp-offset');
         }
-  
+
         $.extend(objOptions, options);
         $.extend(objOptions, attrOptions);
-  
+
         // If class already exists; quit
         if ($obj.data('vp-animated')) {
           return;
         }
-  
+
         // Check offset is percentage based
         if (String(objOptions.offset).indexOf('%') > 0) {
           objOptions.offset = (parseInt(objOptions.offset) / 100) * boxSize.height;
         }
-  
+
         // Get the raw start and end positions
         var rawStart = $obj.offset().top;
-        var rawEnd = rawStart + $obj.height();
-  
+
         // Add the defined offset
         var elemStart = Math.round(rawStart) + objOptions.offset;
         var elemEnd = elemStart + $obj.height();
-  
+
         // Add class if in viewport
         if ((elemStart < viewportEnd) && (elemEnd > viewportStart)) {
           $obj.addClass(objOptions.classToAdd);
@@ -80,22 +79,22 @@
         }
       });
     };
-  
+
     // Select events
     if ('ontouchstart' in window || 'onmsgesturechange' in window) {
       // Device with touchscreen
       $(document).bind('touchmove MSPointerMove pointermove', this.checkElements);
     }
-  
+
     $(options.scrollBox).bind('load scroll', this.checkElements);
-  
+
     $(window).resize(function (e) {
       boxSize = {height: $(options.scrollBox).height(), width: $(options.scrollBox).width()};
       $elem.checkElements();
     });
-  
+
     this.checkElements();
-  
+
     return this;
   };  
 
